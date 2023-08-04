e85bff1b526f4848fef78dcf8a6822121f87a519
Samson Ha <whiteram81@gmail.com>
2023-08-04 13:46:55 -0400

    added getPokemonDescription

--- app/src/main/java/com/example/somethingdex/data/remote/PokeApi.kt
+++ app/src/main/java/com/example/somethingdex/data/remote/PokeApi.kt
@@ -1,6 +1,7 @@
 package com.example.somethingdex.data.remote
 
 import com.example.somethingdex.data.remote.responses.Pokemon
+import com.example.somethingdex.data.remote.responses.SpeciesX
 import retrofit2.http.GET
 import retrofit2.http.Path
 interface PokeApi {
@@ -8,4 +9,9 @@ interface PokeApi {
     suspend fun getPokemonInfo(
         @Path("id") name: Int
     ): Pokemon
+
+    @GET("pokemon-species/{id}")
+    suspend fun getPokemonDescription(
+        @Path("id") name: Int
+    ): SpeciesX
 }
